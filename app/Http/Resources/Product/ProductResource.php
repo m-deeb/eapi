b6f9deb6e78e6569ef0ffe16a4c20490cd9963fd
Mahmoud <m.deeb.work@gmail.com>
2018-04-21 00:46:01 +0200

    Modify product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -9,7 +9,7 @@ class ProductResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
@@ -18,8 +18,14 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount,
+            'stock' => $this->stock == 0 ? 'Out of stock' : $this->stock,
+            'discount' => $this->discount . '%',
+            'totalPrice' => round((1 - ($this->discount/100)) * $this->price, 2),
+            'rating' => $this->reviews->count() > 0 ?
+                round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'Not rated yet',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ],
         ];
     }
 }
